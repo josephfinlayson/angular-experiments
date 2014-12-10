e94a6eaeee75a03629bb5b72a3a84b6014bdd7f1
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-12-10 09:16:49 +0000

    cool hack for using jquery qith angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,90 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+var stateBinder
+
+$.fn.extend({
+    //temporary bind, gets cleared on every state change
+    tbind: function() {
+
+    },
+
+    //angular bind, takes a state and will only be bound on that state
+    abind: function(event, handler, state) {
+        // console.log(this, state, event, handler)
+        console.log(state, event, handler)
+        var $injector = angular.element('body').injector()
+        $injector.get('stateBinder').setEvent(this.selector, state, event, handler)
+    }
+})
+
+$(document).ready(function() {
+    var logEvent = function(event) {
+        console.log("Logged event:", event)
+    }
+
+    $('body').abind('click', logEvent, 'main');
+})
+
+angular.module('experiment', ['ui.router'])
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('main', {
+                url: '/main',
+                templateUrl: '/partial/main.html',
+                controller: 'mainCtrl'
+            })
+            .state('secondary', {
+                url: '/secondary',
+                templateUrl: '/partial/secondary.html',
+                controller: 'secondaryCtrl'
+            })
+
+    })
+    .controller('mainCtrl', function() {
+    })
+    .controller('secondaryCtrl', function() {
+    })
+    .factory('stateBinder', function() {
+        var events
+        var obj = {
+            setEvent: function(selector, state, event, handler) {
+                if (!events) {
+                    events = []
+                }
+                events.push({
+                    selector: selector,
+                    state: state,
+                    event: event,
+                    handler: handler
+                })
+            },
+            getEvents: function() {
+                return events;
+            }
+        }
+        return obj
+    })
+    .run(function($rootScope, stateBinder) {
+        $rootScope.$on('$stateChangeSuccess', function(_event, stateObject) {
+            var events = stateBinder.getEvents()
+
+            var bindEvents = function(event) {
+                if (stateObject.name === event.state) {
+                    $(event.selector).bind(event.event, event.handler)
+                }
+            }
+            if (events) {
+                events.forEach(bindEvents)
+            }
+        })
+
+        $rootScope.$on('$stateChangeStart', function(event, stateObject) {
+            var events = stateBinder.getEvents()
+            var unBindEvents = function(event) {
+                $(event.selector).unbind(event.event, event.handler)
+
+            }
+            if (events) {
+                events.forEach(unBindEvents)
+            }
+        })
+
+    })
